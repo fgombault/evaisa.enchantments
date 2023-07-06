3d0bfedb8d075b668d362a79216783e52ebc497e
François Gombault <francois.gombault@posteo.de>
2023-07-06 22:07:56 +0200

    format

--- files/scripts/player_update.lua
+++ files/scripts/player_update.lua
@@ -6,21 +6,21 @@ local t = GameGetRealWorldTimeSinceStarted();
 local now = GameGetFrameNum();
 
 local player_entity = GetUpdatedEntityID();
-local x, y = EntityGetTransform( player_entity );
+local x, y = EntityGetTransform(player_entity);
 
 if now % 100 == 31 then
 	SetRandomSeed(x + GameGetFrameNum() + StatsGetValue("world_seed"), y + GameGetFrameNum() + StatsGetValue("world_seed"))
-	local card_entities = EntityGetInRadiusWithTag( x, y, 500, "card_action" )
-	for k, v in ipairs(card_entities)do
-		if(not EntityHasTag(v, "has_enchantments"))then
+	local card_entities = EntityGetInRadiusWithTag(x, y, 500, "card_action")
+	for k, v in ipairs(card_entities) do
+		if (not EntityHasTag(v, "has_enchantments")) then
 			local action_comp = EntityGetFirstComponentIncludingDisabled(v, "ItemActionComponent")
-			if(action_comp ~= nil)then
+			if (action_comp ~= nil) then
 				local item_comp = EntityGetFirstComponentIncludingDisabled(v, "ItemComponent")
-				if(item_comp ~= nil and ComponentGetValue2(item_comp, "permanently_attached") == false)then
-				
-					if(HasSettingFlag(FLAGS.enable_random_enchantments))then
-						if(Random(1, 100) < (ModSettingGet(FLAGS.random_enchantment_chance) or 10))then
-							local enchantments_added = AssignRandomEnchantments(v, Random(1, (ModSettingGet(FLAGS.max_random_enchantments) or 2)))
+				if (item_comp ~= nil and ComponentGetValue2(item_comp, "permanently_attached") == false) then
+					if (HasSettingFlag(FLAGS.enable_random_enchantments)) then
+						if (Random(1, 100) < (ModSettingGet(FLAGS.random_enchantment_chance) or 10)) then
+							local enchantments_added = AssignRandomEnchantments(v,
+								Random(1, (ModSettingGet(FLAGS.max_random_enchantments) or 2)))
 							EntityAddTag(v, "has_enchantments")
 						else
 							AppendEnchantmentString(v)
@@ -34,19 +34,19 @@ if now % 100 == 31 then
 			end
 		end
 	end
-	local wand_entities = EntityGetInRadiusWithTag( x, y, 500, "wand" )
-	for k, wand in ipairs(wand_entities)do
+	local wand_entities = EntityGetInRadiusWithTag(x, y, 500, "wand")
+	for k, wand in ipairs(wand_entities) do
 		local cards = EntityGetAllChildren(wand) or {}
-		for k, v in ipairs(cards)do
-			if(not EntityHasTag(v, "has_enchantments") and EntityHasTag(v, "card_action"))then
+		for k, v in ipairs(cards) do
+			if (not EntityHasTag(v, "has_enchantments") and EntityHasTag(v, "card_action")) then
 				local action_comp = EntityGetFirstComponentIncludingDisabled(v, "ItemActionComponent")
-				if(action_comp ~= nil)then
+				if (action_comp ~= nil) then
 					local item_comp = EntityGetFirstComponentIncludingDisabled(v, "ItemComponent")
-					if(item_comp ~= nil and ComponentGetValue2(item_comp, "permanently_attached") == false)then
-						
-						if(HasSettingFlag(FLAGS.enable_random_enchantments))then
-							if(Random(1, 100) < (ModSettingGet(FLAGS.random_enchantment_chance) or 10))then
-								local enchantments_added = AssignRandomEnchantments(v, Random(1, (ModSettingGet(FLAGS.max_random_enchantments) or 2)))
+					if (item_comp ~= nil and ComponentGetValue2(item_comp, "permanently_attached") == false) then
+						if (HasSettingFlag(FLAGS.enable_random_enchantments)) then
+							if (Random(1, 100) < (ModSettingGet(FLAGS.random_enchantment_chance) or 10)) then
+								local enchantments_added = AssignRandomEnchantments(v,
+									Random(1, (ModSettingGet(FLAGS.max_random_enchantments) or 2)))
 								EntityAddTag(v, "has_enchantments")
 							else
 								AppendEnchantmentString(v)
